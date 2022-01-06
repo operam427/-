c32d073c999015a771cda32aaeeddf957f9b2bdd
YuRim Kim <operam427@kakao.com>
2022-01-07 02:18:31 +0900

    create 11021 (A+B - 7)

---
+++ 11021 (A+B - 7).py
@@ -0,0 +1,26 @@
+# < 11021번 > (A+B - 7)
+
+# 두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+
+''' ※ 입력
+첫째 줄에 테스트 케이스의 개수 T가 주어진다.
+각 테스트 케이스는 한 줄로 이루어져 있으며, 각 줄에 A와 B가 주어진다.
+(0 < A, B < 10)
+
+※ 출력
+각 테스트 케이스마다 "Case #x: "를 출력한 다음, A+B를 출력한다. 테스트 케이스 번호는 1부터 시작한다. '''
+
+# 1. 옛날 풀이
+T = int(input())
+
+for i in range(1, T+1):
+    a, b = map(int,input().split())
+    print("Case #" + str(i) + ":" , str(a + b)) # 이 방법 이제 거의 안 씀
+
+# 2. 요즘 풀이
+T = int(input())
+
+for i in range(1, T+1):
+    a,b = map(int,input().split())
+    print("Case #{}: {}".format(i, a + b))
+
