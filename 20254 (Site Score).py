aa29ff548b0a9edb3982ecbc00a68998bd62a8bb
YuRim Kim <operam427@kakao.com>
2022-04-23 23:42:06 +0900

    create 20254번 (Site Score)

---
+++ 20254 (Site Score).py
@@ -0,0 +1,10 @@
+# <20254>
+
+'''
+The site score of 2020 Taipei-Hsinchu regional contest will be 56UR + 24TR + 14UO + 6TO.
+Please write a program to compute the site score of the 2020 Taipei-Hsinchu regional contest.
+'''
+
+a, b, c, d = map(int, input().split())
+
+print(a*56 + b*24 + c*14 + d*6)
